71dbb0b6bf53480667fc9c207f99d3ec62646b41
Alex Harpin <development@landsofshadow.co.uk>
2014-05-10 14:15:46 +0100

    wireless-hostapd: ensure the cipher value given is used by hostapd
    When configuring the wireless interface to use WPA, setting the ciper
    value has no effect on the hostapd configuration file generated,
    resulting in wpa_pairwise always being assigned the default values.
    
    The fix enables setting this value in the generated hostapd file.

--- scripts/wireless-hostapd.pl
+++ scripts/wireless-hostapd.pl
@@ -146,7 +146,7 @@ if ( $config->exists('wep') ) {
 
     print "wpa=", $wpa_mode{$config->returnValue('mode')}, "\n";
 
-    my @cipher = $config->listNodes('cipher');
+    my @cipher = $config->returnValues('cipher');
     @cipher = ( 'TKIP', 'CCMP' )
 	unless (@cipher);
     print "wpa_pairwise=",join(' ',@cipher), "\n";
